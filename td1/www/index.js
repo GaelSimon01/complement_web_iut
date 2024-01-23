4b91a349ab1562d110f31aa9c9c6ecf41bb402a3
gael.simon.28 <gael.simon.28@gmail.com>
2024-01-23 17:55:33 +0100

    tp1

--- td1/www/index.js
+++ td1/www/index.js
@@ -6,17 +6,83 @@ button.addEventListener('click', function(e){
 
 "ECMAScript" == prompt("quel est le nom officiel de javascript") ? alert("bien joué") : alert("non")
 
-let a = +prompt('a?', ''); // + permet de caster en nombre le résultat du prompt
-switch(a){
+switch(+prompt('a?', '')){
 
     case 0:
         alert( 0 );
+        break;
     case 1:
         alert( 1 );
-    case 2,3:
+        break;
+    case 2:
         alert( '2,3' );
-
+        break;
+    case 3 :
+        alert( '2,3' );
+        break;
+    default:
+        alert("nope")
 };
+
+alert(null ?? false);
+let i =0;
+
+do{
+    i++;
+    console.log(i)
+}while(i<10);
+
+
+function pow(x,n){
+    let val=1;
+    for(let i=0;i<n ;i++){
+        val=val*x
+    }
+    return val;
+}
+
+alert(pow(+prompt("valeur a multiplier"),+prompt("la puissance")))
+
+let fruits = ["Apple", "Orange", "Plum"];
+// itère sur des éléments de tableau
+for (let fruit of fruits) {
+  console.log( fruit );
+}
+
+
+var best = ["Javascript","CSS"];
+console.info(best);
+best.push("PHP");
+console.info(best);
+best[Math.ceil((best.length-1)/2)]="SCSS";
+console.info(best);
+alert(best[0]);
+best.push("Python");
+console.info(best);
+
+
+let vals = [];
+function calculSumInput(){
+    let fin = true;
+    while(fin){
+        let input = prompt("entrez valeur");
+        if(isNaN(input) || input=="" || input==null){
+            fin=false;
+        }
+        else{
+            vals.push(parseInt(input));
+            let res=0;
+            for( let num of vals){
+                res+=num;
+            }
+            console.info(res)
+        }
+    }
+
+}
+
+
+calculSumInput();
 
 
 
